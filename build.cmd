b5ca78c5d35a4c7f824e31262f06bda4e2f01ab4
Ilya Seleznev <itsuart@gmail.com>
2016-10-27 02:16:20 +0700

    registering COM server in registry

--- build.cmd
+++ build.cmd
@@ -7,4 +7,5 @@ mkdir bin
 
 REM build server
 gcc -shared -O2 -nostartfiles -Wall --std=c99 -c ./src/server.c -o ./obj/main.obj
-ld -shared --kill-at --no-seh -e entry_point ./obj/main.obj -o ./bin/server.dll -L%LIBRARY_PATH% -lkernel32 -luuid -luser32 -lole32 -lNtosKrnl
+ld -shared --kill-at --no-seh -e entry_point ./obj/main.obj -o ./bin/server.dll -L%LIBRARY_PATH% -lkernel32 -luuid -luser32 -lole32 -lNtosKrnl -ladvapi32
+cp src/register.extension.cmd bin/
--- src/server.c
+++ src/server.c
@@ -9,21 +9,28 @@ typedef uintmax_t uint;
 typedef intmax_t sint;
 typedef USHORT u16;
 
-// {B672C9AD-3FE0-4712-A4BE-4525AD9A7F9F}
-static const GUID SERVER_GUID  = 
-{ 0xb672c9ad, 0x3fe0, 0x4712, { 0xa4, 0xbe, 0x45, 0x25, 0xad, 0x9a, 0x7f, 0x9f } };
+/* GLOBALS */
+
+static const GUID SERVER_GUID =
+    { 0xb672c9ad, 0x3fe0, 0x4712, { 0xa4, 0xbe, 0x45, 0x25, 0xad, 0x9a, 0x7f, 0x9f } };
+
+#define SERVER_GUID_TEXT  L"{B672C9AD-3FE0-4712-A4BE-4525AD9A7F9F}"
 
 static bool is_server_guid(GUID* test){
     return IsEqualGUID(test, &SERVER_GUID);
 }
 
-static void display_last_error(){
-    DWORD last_error = GetLastError();
+static void display_error(DWORD error){
     u16* reason = NULL;
 
-    FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, last_error, 0, (LPWSTR)&reason, 1, NULL);
+    FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, error, 0, (LPWSTR)&reason, 1, NULL);
     MessageBoxW(NULL, reason, NULL, MB_OK);
     HeapFree(GetProcessHeap(), 0, reason);
+}
+
+static void display_last_error(){
+    DWORD last_error = GetLastError();
+    return display_error(last_error);
 }
 
 static uint remove_last_part_from_path(u16* full_path, uint full_path_length){
@@ -66,6 +73,8 @@ static uint server_path_length = 0;
 static u16 server_directory[MAX_UNICODE_PATH_LENGTH + 1] = {0};
 static uint server_directory_length = 0;
 
+
+
 bool WINAPI entry_point(HINSTANCE hInst, DWORD reason, void* pReserved){
     if (reason == DLL_PROCESS_ATTACH){
         server_path_length = GetModuleFileNameW(hInst, (LPWSTR)&server_path, MAX_UNICODE_PATH_LENGTH);
@@ -84,8 +93,63 @@ bool WINAPI entry_point(HINSTANCE hInst, DWORD reason, void* pReserved){
 
 
 
+
 __declspec(dllexport) HRESULT __stdcall DllRegisterServer(void){
-    MessageBoxW(NULL, L"Title", L"DllRegisterServer", MB_OK);
+    static u16 description[] = L"Windows Explorer context menu extension to copy, move and duplicate files and directories\0";
+
+    HKEY clsidKey;
+    LONG errorCode = RegCreateKeyExW(
+         HKEY_CLASSES_ROOT,
+         L"CLSID\\" SERVER_GUID_TEXT,
+         0, NULL,
+         REG_OPTION_NON_VOLATILE,
+         KEY_ALL_ACCESS,
+         NULL,
+         &clsidKey,
+         NULL
+    );
+    if (errorCode != ERROR_SUCCESS){
+       display_error(errorCode);
+       return E_UNEXPECTED;
+    }
+    errorCode = RegSetValueExW(clsidKey, NULL, 0, REG_SZ, (BYTE*)description, sizeof(description));
+    if (errorCode != ERROR_SUCCESS){
+       display_error(errorCode);
+       return E_UNEXPECTED;
+    }
+    
+    HKEY inprocKey;
+    errorCode = RegCreateKeyExW(
+              clsidKey,
+              L"InprocServer32",
+              0, NULL,
+              REG_OPTION_NON_VOLATILE,
+              KEY_ALL_ACCESS,
+              NULL,
+              &inprocKey,
+              NULL              
+    );
+    
+    if (errorCode != ERROR_SUCCESS){
+       display_error(errorCode);
+       return E_UNEXPECTED;
+    }
+
+    errorCode = RegSetValueExW(inprocKey, NULL, 0, REG_SZ, (BYTE*)server_path, (server_path_length + 1) * sizeof(u16));
+    if (errorCode != ERROR_SUCCESS){
+       display_error(errorCode);
+       return E_UNEXPECTED;
+    }
+
+    errorCode = RegSetValueExW(inprocKey, L"ThreadingModel", 0, REG_SZ, L"both", sizeof(L"both") + 2);
+    if (errorCode != ERROR_SUCCESS){
+       display_error(errorCode);
+       return E_UNEXPECTED;
+    }
+    
+
+    RegCloseKey(inprocKey);
+    RegCloseKey(clsidKey);
     return S_OK;
 }
 
