1385a892e6a0a794fc3ee6c599a27b55ee2732cf
Ilya Seleznev <itsuart@gmail.com>
2016-10-27 08:03:45 +0700

    renamed server to extension

--- build.cmd
+++ build.cmd
@@ -5,7 +5,6 @@ RD /S /Q bin
 mkdir obj
 mkdir bin
 
-REM build server
-gcc -shared -O2 -nostartfiles -Wall --std=c99 -c ./src/server.c -o ./obj/main.obj
-ld -shared --kill-at --no-seh -e entry_point ./obj/main.obj -o ./bin/server.dll -L%LIBRARY_PATH% -lkernel32 -luuid -luser32 -lole32 -lNtosKrnl -ladvapi32
-cp src/register.extension.cmd bin/
+REM build extension
+gcc -shared -O2 -nostartfiles -Wall --std=c99 -c ./src/extension.c -o ./obj/extension.obj
+ld -shared --kill-at --no-seh -e entry_point ./obj/extension.obj -o ./bin/extension.dll -L%LIBRARY_PATH% -lkernel32 -luuid -luser32 -lole32 -lNtosKrnl -ladvapi32
