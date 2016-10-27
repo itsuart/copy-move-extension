83b8ff948eab9b594ec56b3b62b2f0300f8c642b
Ilya Seleznev <itsuart@gmail.com>
2016-10-27 18:40:37 +0700

    IUnknown implemented

--- build.cmd
+++ build.cmd
@@ -5,10 +5,13 @@ RD /S /Q bin
 mkdir obj
 mkdir bin
 
-REM build extension
+echo BUILDING EXTENSION
+echo ------------------
 gcc -shared -O2 -nostartfiles -Wall --std=c99 -c ./src/extension.c -o ./obj/extension.obj
-ld -shared --kill-at --no-seh -e entry_point ./obj/extension.obj -o ./bin/extension.dll -L%LIBRARY_PATH% -lkernel32 -luuid -luser32 -lole32 -lNtosKrnl -ladvapi32
+ld -shared --kill-at --no-seh ./obj/extension.obj -o ./bin/extension.dll -L%LIBRARY_PATH% -lkernel32 -luuid -luser32 -lole32 -lNtosKrnl -ladvapi32
 
-REM build installer
+echo
+echo BUILDING INSTALLER
+echo ------------------
 gcc -O2 -nostartfiles -Wall --std=c99 -c ./src/installer.c -o ./obj/installer.obj
 ld -s --subsystem windows --kill-at --no-seh -e entry_point ./obj/installer.obj -o "./bin/in staller.exe"  -L%LIBRARY_PATH% -lkernel32 -luuid -luser32 -lole32 -lNtosKrnl -ladvapi32 -lshell32
--- src/extension.c
+++ src/extension.c
@@ -36,13 +36,170 @@ static void close_log_file(){
     }
 }
 
+typedef struct {
+    IUnknownVtbl* lpVtbl;
+    long int refsCount;
+} IMyObj;
 
+long int nObjectsAndRefs = 0;
 
-bool WINAPI entry_point(HINSTANCE hInst, DWORD reason, void* pReserved){
-    return true;
+ULONG STDMETHODCALLTYPE myObj_AddRef(IMyObj* pMyObj){
+    InterlockedIncrement(&nObjectsAndRefs);
+
+    InterlockedIncrement(&(pMyObj->refsCount));
+    return pMyObj->refsCount;
+}
+
+ULONG STDMETHODCALLTYPE myObj_Release(IMyObj* pMyObj){
+    InterlockedDecrement(&nObjectsAndRefs);
+
+    InterlockedDecrement(&(pMyObj->refsCount));
+    long int nRefs = pMyObj->refsCount;
+
+    if (nRefs == 0){
+        GlobalFree(pMyObj);
+    }
+
+    return nRefs;
+}
+
+HRESULT STDMETHODCALLTYPE
+myObj_QueryInterface(IMyObj* pMyObj, REFIID requestedIID, void **ppv){
+    if (ppv == NULL){
+        return E_POINTER;
+    }
+    *ppv = NULL;
+
+    if (! IsEqualGUID(requestedIID, &IID_IUnknown)){
+        return E_NOINTERFACE;
+    }
+
+    *ppv = pMyObj;
+    pMyObj->lpVtbl->AddRef(pMyObj);
+    return S_OK;
+}
+
+static IUnknownVtbl IMyObjVtbl = {
+    .QueryInterface = &myObj_QueryInterface,
+    .AddRef = &myObj_AddRef,
+    .Release = &myObj_Release
 };
 
-__declspec(dllexport) HRESULT __stdcall DllRegisterServer(void){
+HRESULT STDMETHODCALLTYPE classCreateInstance(
+        IClassFactory* pClassFactory, IUnknown* punkOuter, REFIID pRequestedIID, void** ppv){
+    if (ppv == NULL){
+        return E_POINTER;
+    }
+
+   // Assume an error by clearing caller's handle.
+   *ppv = 0;
+
+   // We don't support aggregation in IExample.
+   if (punkOuter){
+       return CLASS_E_NOAGGREGATION;
+   }
+
+   if (! IsEqualGUID(pRequestedIID, &IID_IUnknown)){
+       return E_NOINTERFACE;
+   }
+
+   IMyObj* pMyObj = GlobalAlloc(GMEM_FIXED, sizeof(IMyObj));
+   if (pMyObj == NULL){
+       return E_OUTOFMEMORY;
+   }
+   
+   pMyObj->lpVtbl = &IMyObjVtbl;
+   pMyObj->refsCount = 0;
+   pMyObj->lpVtbl->AddRef(pMyObj);
+
+
+   *ppv = pMyObj;
+   return S_OK;
+}
+
+ULONG STDMETHODCALLTYPE classAddRef(IClassFactory* pClassFactory){
+   return 1;
+}
+
+ULONG STDMETHODCALLTYPE classRelease(IClassFactory* pClassFactory){
+   return 1;
+}
+
+long int nLocks = 0;
+
+HRESULT STDMETHODCALLTYPE classLockServer(IClassFactory* pClassFactory, BOOL flock){
+    if (flock){
+        InterlockedIncrement(&nLocks);
+    } else {
+        InterlockedDecrement(&nLocks);
+    }
+
     return S_OK;
+}
+
+HRESULT STDMETHODCALLTYPE
+classQueryInterface(IClassFactory* pClassFactory, REFIID requestedIID, void **ppv){
+   // Check if the GUID matches an IClassFactory or IUnknown GUID.
+   if (! IsEqualGUID(requestedIID, &IID_IUnknown) && 
+       ! IsEqualGUID(requestedIID, &IID_IClassFactory)){
+      // It doesn't. Clear his handle, and return E_NOINTERFACE.
+      *ppv = 0;
+      return E_NOINTERFACE;
+   }
+
+   // First, we fill in his handle with the same object pointer he passed us.
+   // That's our IClassFactory (MyIClassFactoryObj) he obtained from us.
+   *ppv = pClassFactory;
+
+   // Call our IClassFactory's AddRef, passing the IClassFactory. 
+   pClassFactory->lpVtbl->AddRef(pClassFactory);
+
+   // Let him know he indeed has an IClassFactory. 
+   return S_OK;
+}
+
+static const IClassFactoryVtbl IClassFactory_Vtbl = {
+    classQueryInterface,
+    classAddRef,
+    classRelease,
+    classCreateInstance,
+    classLockServer
+};
+
+//since we need only one ClassFactory ever:
+static IClassFactory IClassFactoryObj = {
+    &IClassFactory_Vtbl
+};
+
+/*
+  COM subsystem will call this function in attempt to retrieve an implementation of an IID.
+  Usually it's IClassFactory that is requested (but could be anything else?)
+ */
+__declspec(dllexport) HRESULT __stdcall DllGetClassObject(REFCLSID pCLSID, REFIID pIID, void** ppv){
+    if (ppv == NULL){
+        return E_POINTER;
+    }
+
+    *ppv = NULL;
+    if (! is_server_guid(pCLSID)){
+        //that's not our CLSID, ignore
+        return CLASS_E_CLASSNOTAVAILABLE;
+    }
+
+    if (! IsEqualGUID(pIID, &IID_IClassFactory)){
+        //we only providing ClassFactories in this function
+        return CLASS_E_CLASSNOTAVAILABLE;
+    }
+
+    //alright, return our implementation of IClassFactory
+    *ppv = &IClassFactoryObj;
+    return S_OK;
+}
+
+__declspec(dllexport) HRESULT PASCAL DllCanUnloadNow(){
+    if (nLocks == 0 && nObjectsAndRefs == 0){
+        return S_OK;
+    }
+    return S_FALSE;
 }
 
