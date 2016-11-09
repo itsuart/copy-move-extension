b117b8bf7119dcac0e4d24d7096abff1c3af7aca
Ilya Seleznev <itsuart@gmail.com>
2016-11-09 15:32:03 +0700

    reformatted code
    in futile attempt to get rid of warning: initialization from
    incompatible pointer type [-Wincompatible-pointer-types]

--- src/extension.c
+++ src/extension.c
@@ -12,22 +12,22 @@
 typedef uintmax_t uint;
 typedef intmax_t sint;
 typedef USHORT u16;
+typedef unsigned char u8;
 
 /* GLOBALS */
 #include "common.c"
 
-static bool is_server_guid(GUID* test){
-    return IsEqualGUID(test, &SERVER_GUID);
-}
-
 /* FORWARD DECLARATION EVERYTHING */
 struct tag_IMyObj;
 
+ULONG STDMETHODCALLTYPE myObj_AddRef(struct tag_IMyObj* pMyObj);
+ULONG STDMETHODCALLTYPE myObj_Release(struct tag_IMyObj* pMyObj);
+HRESULT STDMETHODCALLTYPE myObj_QueryInterface(struct tag_IMyObj* pMyObj, REFIID requestedIID, void **ppv);
+
+/* ALL THE STRUCTS (because it's too much pain to have them in separate places) */
 typedef struct tag_MyIContextMenuImpl {
     IContextMenuVtbl* lpVtbl;
     struct tag_IMyObj* pBase;
-    u16 copyLabelBuffer[20 + (sizeof(L"COPY ")/sizeof(L' ')) + (sizeof(L" items to...")/sizeof(L' ')) + 1];
-    u16 moveLabelBuffer[20 + (sizeof(L"MOVE ")/sizeof(L' ')) + (sizeof(L" items to...")/sizeof(L' ')) + 1];
 } MyIContextMenuImpl;
 
 typedef struct tag_MyIShellExtInitImpl {
@@ -44,12 +44,240 @@ typedef struct tag_IMyObj{
     long int refsCount;
 } IMyObj;
 
+static void cleanup_names_storage(IMyObj* pBase){
+    if (pBase->itemNames != NULL){
+        GlobalFree(pBase->itemNames);
+    }
+    pBase->itemNames = NULL;
+    pBase->nItems = 0;
+}
 
-ULONG STDMETHODCALLTYPE myObj_AddRef(IMyObj* pMyObj);
-ULONG STDMETHODCALLTYPE myObj_Release(IMyObj* pMyObj);
-HRESULT STDMETHODCALLTYPE myObj_QueryInterface(IMyObj* pMyObj, REFIID requestedIID, void **ppv);
 
-/* BODY */
+
+/******************************* IContextMenu ***************************/
+static const u16* COPY_VERB = SERVER_GUID_TEXT L".COPY";
+static const u16* MOVE_VERB = SERVER_GUID_TEXT L".MOVE";
+
+HRESULT STDMETHODCALLTYPE myIContextMenuImpl_GetCommandString(MyIContextMenuImpl* pImpl, 
+                                                              UINT_PTR idCmd, UINT uFlags, UINT* pwReserved,LPSTR pszName,UINT cchMax){
+    //not really in use since Vista and we don't provide canonical verbs
+    OutputDebugStringW(L"GetCommandString");
+    return E_FAIL;
+    //    return S_OK;
+}
+
+#define COPY_TO_MENU_OFFSET 1
+#define MOVE_TO_MENU_OFFSET 2
+
+//returns number of digits in the number
+static uint u64toW(uint number, u16 stringBuffer[20]){
+    const static u16 DIGITS[10] = L"0123456789";
+    if (number == 0){ //special case
+        stringBuffer[0] = L'0';
+        return 1;
+    }
+
+    //biggest 64 bit unsigned number is 18,446,744,073,709,551,615 that is 20 digits, so
+    uint divisor = 10;
+    int power = 0;
+    u8 reminders[20] = {0};
+
+    for (; power < 20; power += 1){
+        char reminder = number % divisor;
+
+        reminders[power] = reminder;
+        number = number / divisor;
+
+        if (number == 0){
+            break;
+        }
+    }
+
+    //put 'em digits in correct order
+    for (int i = power; i >= 0; i -= 1){
+        stringBuffer[power - i] = DIGITS[reminders[i]];
+    }
+
+    return power;
+}
+
+static void mk_label (u16* prefix, uint number, u16* buffer){
+    uint index = 0;
+    while (prefix[index] != 0){
+        buffer[index] = prefix[index];
+        index += 1;
+    }
+    uint nDigits = u64toW(number, buffer + index);
+    index += nDigits;
+
+    lstrcpyW(buffer + index + 1, number > 1 ? L" items to..." : L" item to...");
+}
+
+
+//I really hate person that causes me "undefined reference to `___chkstk_ms'" errors. And extra calls to clear the memory.
+static u16 targetDir[MAX_UNICODE_PATH_LENGTH + 2]; //extra 1 for trailing zero
+HRESULT STDMETHODCALLTYPE myIContextMenuImpl_InvokeCommand(MyIContextMenuImpl* pImpl, LPCMINVOKECOMMANDINFO pCommandInfo){
+    IMyObj* pBase = pImpl->pBase;
+    
+    OutputDebugStringW(L"FOO");
+
+    if (pBase->nItems == 0){
+        return S_OK; //instantly copy or move nothing!
+    }
+    OutputDebugStringW(L"BAR");
+
+    const char* pVerb = pCommandInfo->lpVerb;
+
+    if (! IS_INTRESOURCE(pVerb)){
+        cleanup_names_storage(pBase);
+        return E_FAIL;
+    }
+    OutputDebugStringW(L"BAZ");
+    
+    u16 title[(sizeof(L"COPY ")/sizeof(L' ')) + 20 + (sizeof(L" items to...")/sizeof(L' ')) + 1];
+    SecureZeroMemory(&title[0], sizeof(title));    
+    uint fileOp;
+
+    if ((void*)pVerb == (void*)MAKEINTRESOURCE(COPY_TO_MENU_OFFSET)){
+        mk_label(L"COPY ", pBase->nItems, &title[0]);
+        fileOp = FO_COPY;
+    } else if ((void*)pVerb == (void*)MAKEINTRESOURCE(MOVE_TO_MENU_OFFSET)){
+        mk_label(L"MOVE ", pBase->nItems, &title[0]);
+        fileOp = FO_MOVE;
+    } else {
+        cleanup_names_storage(pBase);
+        return E_FAIL;
+    }
+
+    OutputDebugStringW(L"BAZ+");
+    //I want my monads in C!
+    HRESULT hr = S_OK; 
+    // Create a new common open file dialog.
+    IFileOpenDialog *pfd = NULL;
+    hr = CoCreateInstance(&CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER, &IID_IFileOpenDialog, (void**)&pfd);
+    if (SUCCEEDED(hr)){
+        OutputDebugStringW(L"BAZ++");
+        // Set the dialog as a folder picker.
+        DWORD dwOptions;
+        hr = pfd->lpVtbl->GetOptions(pfd, &dwOptions);
+        if (SUCCEEDED(hr)){
+            hr = pfd->lpVtbl->SetOptions(pfd, dwOptions | FOS_PICKFOLDERS);
+
+            // Set the title of the dialog.
+            if (SUCCEEDED(hr)){
+                hr = pfd->lpVtbl->SetTitle(pfd, &title[0]);
+
+                // Show the open file dialog.
+                if (SUCCEEDED(hr)){
+                    hr = pfd->lpVtbl->Show(pfd, pCommandInfo->hwnd);
+                    if (SUCCEEDED(hr)){
+                        // Get the selection from the user.
+                        IShellItem *psiResult = NULL;
+                        hr = pfd->lpVtbl->GetResult(pfd, &psiResult);
+                        if (SUCCEEDED(hr)){
+                            u16* pszPath = NULL;
+                            hr = psiResult->lpVtbl->GetDisplayName(psiResult, SIGDN_FILESYSPATH, &pszPath);
+                            if (SUCCEEDED(hr)){
+                                SecureZeroMemory(targetDir, sizeof(targetDir));
+                                lstrcpyW(&targetDir[0], pszPath);
+                                CoTaskMemFree(pszPath);
+
+                                SHFILEOPSTRUCT shFileOp = {
+                                    .hwnd = pCommandInfo->hwnd,
+                                    .wFunc = fileOp,
+                                    .pFrom = pBase->itemNames,
+                                    .pTo = &targetDir[0],
+                                    .fFlags = FOF_ALLOWUNDO,
+                                    .fAnyOperationsAborted = false,
+                                    .hNameMappings = NULL,
+                                    .lpszProgressTitle = L"ERROR!"
+                                };
+
+                                SHFileOperation(&shFileOp);                        
+                            }
+                            psiResult->lpVtbl->Release(psiResult);
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+
+
+    pfd->lpVtbl->Release(pfd);
+    cleanup_names_storage(pBase);
+    return S_OK;
+}
+
+
+HRESULT STDMETHODCALLTYPE myIContextMenuImpl_QueryContextMenu(MyIContextMenuImpl* pImpl, 
+                                                           HMENU hmenu, UINT indexMenu, UINT idCmdFirst, UINT idCmdLast, UINT uFlags){
+    IMyObj* pBase = pImpl->pBase;
+    
+    if (CMF_DEFAULTONLY == (CMF_DEFAULTONLY & uFlags)
+        || CMF_VERBSONLY == (CMF_VERBSONLY & uFlags)
+        || CMF_NOVERBS == (CMF_NOVERBS & uFlags)){
+
+        return MAKE_HRESULT(SEVERITY_SUCCESS, 0, 0);
+    }
+
+    //in highly unlikely case where is no room left in the menu:
+    if (idCmdFirst + 2 > idCmdLast){
+        //we don't add any menu enries of ours
+        cleanup_names_storage(pBase);
+        return MAKE_HRESULT(SEVERITY_SUCCESS, 0, 0);
+    }
+
+    //insert our menu items: separator, copy and move in that order.
+    InsertMenu(hmenu, -1, MF_BYPOSITION | MF_SEPARATOR, idCmdFirst + 0, NULL);
+    InsertMenu(hmenu, -1, MF_BYPOSITION | MF_STRING, idCmdFirst + COPY_TO_MENU_OFFSET, L"Copy To...");
+    InsertMenu(hmenu, -1, MF_BYPOSITION | MF_STRING, idCmdFirst + MOVE_TO_MENU_OFFSET, L"Move To...");
+
+    OutputDebugStringW(L"QueryContextMenu");
+
+    return MAKE_HRESULT(SEVERITY_SUCCESS, 0, MOVE_TO_MENU_OFFSET + 1); //shouldn't this be +idCmdFirst?
+}
+
+
+ULONG STDMETHODCALLTYPE myIContextMenuImpl_AddRef(MyIContextMenuImpl* pImpl){
+    return myObj_AddRef(pImpl->pBase);
+}
+
+ULONG STDMETHODCALLTYPE myIContextMenuImpl_Release(MyIContextMenuImpl* pImpl){
+    return myObj_Release(pImpl->pBase);
+}
+
+HRESULT STDMETHODCALLTYPE myIContextMenuImpl_QueryInterface(MyIContextMenuImpl* pImpl, REFIID requestedIID, void **ppv){
+    if (ppv == NULL){
+        return E_POINTER;
+    }
+
+    *ppv = NULL;
+
+    if (IsEqualGUID(requestedIID, &IID_IUnknown)){
+        *ppv = pImpl;
+        myObj_AddRef(pImpl->pBase);
+        return S_OK;
+    }
+
+    return myObj_QueryInterface(pImpl->pBase, requestedIID, ppv);
+
+}
+
+static IContextMenuVtbl IMyIContextMenuVtbl = {
+    .QueryInterface = &myIContextMenuImpl_QueryInterface,
+    .AddRef = &myIContextMenuImpl_AddRef,
+    .Release = &myIContextMenuImpl_Release,
+    .GetCommandString = &myIContextMenuImpl_GetCommandString,
+    .QueryContextMenu = &myIContextMenuImpl_QueryContextMenu,
+    .InvokeCommand = &myIContextMenuImpl_InvokeCommand
+};
+
+
+
+/************************ IShellExtInit **********************************/
 HRESULT STDMETHODCALLTYPE myIShellExtInit_AddRef(MyIShellExtInitImpl* pImpl){
     return myObj_AddRef(pImpl->pBase);
 }
@@ -122,226 +350,10 @@ static IShellExtInitVtbl IMyIShellExtVtbl = {
     .Initialize = &myIShellExtInit_Initialize
 };
 
-static const u16* COPY_VERB = SERVER_GUID_TEXT L".COPY";
-static const u16* MOVE_VERB = SERVER_GUID_TEXT L".MOVE";
-
-HRESULT STDMETHODCALLTYPE myIContextMenuImpl_GetCommandString(MyIContextMenuImpl* pImpl, 
-                                                              UINT_PTR idCmd, UINT uFlags, UINT* pwReserved,LPSTR pszName,UINT cchMax){
-    //not really in use since Vista and we don't provide canonical verbs
-    OutputDebugStringW(L"GetCommandString");
-    return E_FAIL;
-    //    return S_OK;
-}
-
-#define COPY_TO_MENU_OFFSET 1
-#define MOVE_TO_MENU_OFFSET 2
-
-static void cleanup_names_storage(IMyObj* pBase){
-    if (pBase->itemNames != NULL){
-        GlobalFree(pBase->itemNames);
-    }
-    pBase->itemNames = NULL;
-    pBase->nItems = 0;
-}
-
-//I really hate person that causes me "undefined reference to `___chkstk_ms'" errors. And extra calls to clear the memory.
-static u16 targetDir[MAX_UNICODE_PATH_LENGTH + 2]; //extra 1 for trailing zero
-HRESULT STDMETHODCALLTYPE myIContextMenuImpl_InvokeCommand(MyIContextMenuImpl* pImpl, LPCMINVOKECOMMANDINFO pCommandInfo){
-    IMyObj* pBase = pImpl->pBase;
-    
-    OutputDebugStringW(L"FOO");
-
-    if (pBase->nItems == 0){
-        return S_OK; //instantly copy or move nothing!
-    }
-    OutputDebugStringW(L"BAR");
-
-    const char* pVerb = pCommandInfo->lpVerb;
-
-    if (! IS_INTRESOURCE(pVerb)){
-        cleanup_names_storage(pBase);
-        return E_FAIL;
-    }
-    OutputDebugStringW(L"BAZ");
-    
-    u16* title = NULL;
-    uint fileOp;
-    
-    if ((void*)pVerb == (void*)MAKEINTRESOURCE(COPY_TO_MENU_OFFSET)){
-        title  = &pImpl->copyLabelBuffer[0];
-        fileOp = FO_COPY;
-    } else if ((void*)pVerb == (void*)MAKEINTRESOURCE(MOVE_TO_MENU_OFFSET)){
-        title =  &pImpl->moveLabelBuffer[0];
-        fileOp = FO_MOVE;
-    } else {
-        cleanup_names_storage(pBase);
-        return E_FAIL;
-    }
-    OutputDebugStringW(L"BAZ+");
-    //I want my monads in C!
-    HRESULT hr = S_OK; 
-    // Create a new common open file dialog.
-    IFileOpenDialog *pfd = NULL;
-    hr = CoCreateInstance(&CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER, &IID_IFileOpenDialog, (void**)&pfd);
-    if (SUCCEEDED(hr)){
-    OutputDebugStringW(L"BAZ++");
-        // Set the dialog as a folder picker.
-        DWORD dwOptions;
-        hr = pfd->lpVtbl->GetOptions(pfd, &dwOptions);
-        if (SUCCEEDED(hr)){
-            hr = pfd->lpVtbl->SetOptions(pfd, dwOptions | FOS_PICKFOLDERS);
-        }
-
-        // Set the title of the dialog.
-        if (SUCCEEDED(hr)){
-            hr = pfd->lpVtbl->SetTitle(pfd, title);
-        }
-
-        // Show the open file dialog.
-        if (SUCCEEDED(hr)){
-            hr = pfd->lpVtbl->Show(pfd, pCommandInfo->hwnd);
-            if (SUCCEEDED(hr)){
-                // Get the selection from the user.
-                IShellItem *psiResult = NULL;
-                hr = pfd->lpVtbl->GetResult(pfd, &psiResult);
-                if (SUCCEEDED(hr)){
-                    u16* pszPath = NULL;
-                    hr = psiResult->lpVtbl->GetDisplayName(psiResult, SIGDN_FILESYSPATH, &pszPath);
-                    if (SUCCEEDED(hr)){
-                        SecureZeroMemory(targetDir, sizeof(targetDir));
-                        lstrcpyW(&targetDir[0], pszPath);
-                        CoTaskMemFree(pszPath);
-
-                        SHFILEOPSTRUCT shFileOp = {
-                            .hwnd = pCommandInfo->hwnd,
-                            .wFunc = fileOp,
-                            .pFrom = pBase->itemNames,
-                            .pTo = &targetDir[0],
-                            .fFlags = FOF_ALLOWUNDO,
-                            .fAnyOperationsAborted = false,
-                            .hNameMappings = NULL,
-                            .lpszProgressTitle = L"ERROR!"
-                        };
-
-                        SHFileOperation(&shFileOp);                        
-                    }
-                    psiResult->lpVtbl->Release(psiResult);
-                }
-            }
-        }
-    }
-
-    pfd->lpVtbl->Release(pfd);
-    cleanup_names_storage(pBase);
-    return S_OK;
-}
-
-//returns number of digits in the number
-uint u64toW(uint number, u16 stringBuffer[20]){
-    const static u16 DIGITS[10] = L"0123456789";
-    if (number == 0){ //special case
-        *stringBuffer = L'0';
-        return 1;
-    }
-
-    //biggest 64 bit unsigned number is 18,446,744,073,709,551,615 that is 20 digits, so
-    uint divisor = 10;
-    int power = 0;
-    char reminders[20] = {0};
-
-    for (; power < 20; power += 1){
-        char reminder = number % divisor;
-
-        reminders[power] = reminder;
-        number = number / divisor;
-
-        if (number == 0){
-            break;
-        }
-    }
-
-    //put 'em digits in correct order
-    for (int i = power; i >= 0; i -= 1){
-        stringBuffer[power - i] = DIGITS[reminders[i]];
-    }
-
-    return power;
-}
-
-void mk_label (u16* prefix, uint number, u16* buffer){
-    uint index = 0;
-    while (prefix[index] != 0){
-        buffer[index] = prefix[index];
-        index += 1;
-    }
-    uint nDigits = u64toW(number, buffer + index);
-    index += nDigits;
-
-    lstrcpyW(buffer + index + 1, number > 1 ? L" items to..." : L" item to...");
-}
-
-HRESULT STDMETHODCALLTYPE myIContextMenuImpl_QueryContextMenu(MyIContextMenuImpl* pImpl, 
-                                                           HMENU hmenu, UINT indexMenu, UINT idCmdFirst, UINT idCmdLast, UINT uFlags){
-
-    IMyObj* pBase = pImpl->pBase;
-    
-    if (CMF_DEFAULTONLY == (CMF_DEFAULTONLY & uFlags)
-        || CMF_VERBSONLY == (CMF_VERBSONLY & uFlags)
-        || CMF_NOVERBS == (CMF_NOVERBS & uFlags)){
-
-        return MAKE_HRESULT(SEVERITY_SUCCESS, 0, 0);
-    }
-
-    //in highly unlikely case where is no room left in the menu:
-    if (idCmdFirst + 2 > idCmdLast){
-        //we don't add any menu enries of ours
-        cleanup_names_storage(pBase);
-        return MAKE_HRESULT(SEVERITY_SUCCESS, 0, 0);
-    }
-
-    SecureZeroMemory(pImpl->copyLabelBuffer, sizeof(pImpl->copyLabelBuffer));
-    mk_label(L"COPY ", pBase->nItems, &pImpl->copyLabelBuffer[0]);
-
-    SecureZeroMemory(pImpl->moveLabelBuffer, sizeof(pImpl->moveLabelBuffer));
-    mk_label(L"MOVE ", pBase->nItems, &pImpl->moveLabelBuffer[0]);
-
-    //insert our menu items: separator, copy and move in that order.
-    InsertMenu(hmenu, -1, MF_BYPOSITION | MF_SEPARATOR, idCmdFirst + 0, NULL);
-    InsertMenu(hmenu, -1, MF_BYPOSITION | MF_STRING, idCmdFirst + COPY_TO_MENU_OFFSET, &pImpl->copyLabelBuffer[0]);
-    InsertMenu(hmenu, -1, MF_BYPOSITION | MF_STRING, idCmdFirst + MOVE_TO_MENU_OFFSET, &pImpl->moveLabelBuffer[0]);
-
-    OutputDebugStringW(L"QueryContextMenu");
-
-    return MAKE_HRESULT(SEVERITY_SUCCESS, 0, MOVE_TO_MENU_OFFSET + 1); //shouldn't this be +idCmdFirst?
-}
 
 
-ULONG STDMETHODCALLTYPE myIContextMenuImpl_AddRef(MyIContextMenuImpl* pImpl){
-    return myObj_AddRef(pImpl->pBase);
-}
-
-ULONG STDMETHODCALLTYPE myIContextMenuImpl_Release(MyIContextMenuImpl* pImpl){
-    return myObj_Release(pImpl->pBase);
-}
-
-HRESULT STDMETHODCALLTYPE myIContextMenuImpl_QueryInterface(MyIContextMenuImpl* pImpl, REFIID requestedIID, void **ppv){
-    if (ppv == NULL){
-        return E_POINTER;
-    }
-
-    *ppv = NULL;
-
-    if (IsEqualGUID(requestedIID, &IID_IUnknown)){
-        *ppv = pImpl;
-        myObj_AddRef(pImpl->pBase);
-        return S_OK;
-    }
-
-    return myObj_QueryInterface(pImpl->pBase, requestedIID, ppv);
-
-}
-
-long int nObjectsAndRefs = 0;
+/******************* IMyObj  *******************************/
+static long int nObjectsAndRefs = 0;
 
 ULONG STDMETHODCALLTYPE myObj_AddRef(IMyObj* pMyObj){
     InterlockedIncrement(&nObjectsAndRefs);
@@ -404,16 +416,9 @@ static IUnknownVtbl IMyObjVtbl = {
     .Release = &myObj_Release
 };
 
-static IContextMenuVtbl IMyIContextMenuVtbl = {
-    .QueryInterface = &myIContextMenuImpl_QueryInterface,
-    .AddRef = &myIContextMenuImpl_AddRef,
-    .Release = &myIContextMenuImpl_Release,
-    .GetCommandString = &myIContextMenuImpl_GetCommandString,
-    .QueryContextMenu = &myIContextMenuImpl_QueryContextMenu,
-    .InvokeCommand = &myIContextMenuImpl_InvokeCommand
-};
 
 
+/************** IClassFactory *****************************/
 HRESULT STDMETHODCALLTYPE classCreateInstance(IClassFactory* pClassFactory, IUnknown* punkOuter, REFIID pRequestedIID, void** ppv){
     if (ppv == NULL){
         return E_POINTER;
@@ -470,8 +475,7 @@ HRESULT STDMETHODCALLTYPE classLockServer(IClassFactory* pClassFactory, BOOL flo
     return S_OK;
 }
 
-HRESULT STDMETHODCALLTYPE
-classQueryInterface(IClassFactory* pClassFactory, REFIID requestedIID, void **ppv){
+HRESULT STDMETHODCALLTYPE classQueryInterface(IClassFactory* pClassFactory, REFIID requestedIID, void **ppv){
    // Check if the GUID matches an IClassFactory or IUnknown GUID.
    if (! IsEqualGUID(requestedIID, &IID_IUnknown) && 
        ! IsEqualGUID(requestedIID, &IID_IClassFactory)){
@@ -495,8 +499,11 @@ static const IClassFactoryVtbl IClassFactory_Vtbl = {
 
 //since we need only one ClassFactory ever:
 static IClassFactory IClassFactoryObj = {
-    &IClassFactory_Vtbl
+    .lpVtbl = &IClassFactory_Vtbl
 };
+
+
+/************************** DLL STUFF ****************************************/
 
 /*
   COM subsystem will call this function in attempt to retrieve an implementation of an IID.
@@ -508,7 +515,7 @@ __declspec(dllexport) HRESULT __stdcall DllGetClassObject(REFCLSID pCLSID, REFII
     }
 
     *ppv = NULL;
-    if (! is_server_guid(pCLSID)){
+    if (! IsEqualGUID(pCLSID, &SERVER_GUID)){
         //that's not our CLSID, ignore
         return CLASS_E_CLASSNOTAVAILABLE;
     }
